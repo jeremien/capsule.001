506af97fa1db13d30b243d049a4f2509d1512c93
jeremie nuel <jeremie.nuel@gmail.com>
2021-10-05 11:55:08 +0200

    mise à jour

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask import (
                     render_template,
-                    send_from_directory
-                  ) 
+                    send_from_directory,
+                    json,
+                    jsonify,
+                    request
+                  )
+from flask.wrappers import Request 
 
 app = Flask(__name__)
 app.debug = True
@@ -14,9 +19,16 @@ data = [
         'lastname':'jacob'},
        ]
 
+def open_file(file, type):
+  with app.open_resource(f'data/{file}') as d:
+    if type == 'json':
+      return json.load(d)
+    else:
+      return d.read()
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', content='salut')
 
 @app.route('/hello/<name>')
 def page(name):
@@ -35,6 +47,32 @@ def users():
 @app.route('/dataplus')
 def usersplus():
   return render_template('users.html', data=data)
+
+@app.route('/api')
+def api():
+  d = open_file('data.json', type='json')
+  return jsonify(d)
+  # return jsonify(data)
+
+@app.route('/txt')
+def text():
+  d = open_file('data.txt', type='texte')
+  return d
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+  
+  if request.method == 'GET':
+    print('GET')
+    return render_template('form.html')
+  
+  if request.method == 'POST':
+    print('POST')
+    txt_file = os.path.join("data","data.txt")
+    nom = request.form['nom']
+    with open(txt_file, 'w') as file:
+      file.write(nom)
+    return render_template('home.html', content='nom enregistré')
 
 @app.route("/static/<path:path>")
 def static_dir(path):
